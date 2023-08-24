e981d44b78f48521f8071e547617c2342bdeb22c
Jade Rowland <jaderowlanddev@gmail.com>
2023-08-23 20:13:14 -0400

    still figuring things out

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,34 +1,27 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use log::warn;
-use tauri_plugin_log::{LogTarget};
+use tauri_plugin_log::{ LogTarget };
 mod midi;
 
-// fn main() {
-//   tauri::Builder::default()
-//     .run(tauri::generate_context!())
-//     .expect("error while running tauri application");
-//   warn!("leeeroy jeeenkins");
-
-//   println!("asfaijasfajsfiajfs");
-//   let x = 2;
-//   let  _y = 2 + x;
-//   midi::initialize();
-// }
-
 fn main() {
-  tauri::Builder::default()
-      .plugin(tauri_plugin_log::Builder::default().targets([
-        // LogTarget::LogDir,
+  tauri::Builder
+    ::default()
+    .plugin(
+      tauri_plugin_log::Builder
+        ::default()
+        .targets([
+          // LogTarget::LogDir,
           LogTarget::Stdout,
-         // LogTarget::Webview,
-      ]).build())
-     .setup(|_app| {
+          // LogTarget::Webview,
+        ])
+        .build()
+    )
+    .setup(|_app| {
       warn!("Setting up...");
-      midi::initialize();
+      midi::init();
       Ok(())
-     })
-      .run(tauri::generate_context!())
-      .expect("error while running tauri application");
-
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
--- src-tauri/src/midi.rs
+++ src-tauri/src/midi.rs
@@ -1,31 +1,77 @@
-use std::sync::{Arc, Mutex};
-use midir::{Ignore, MidiInput, MidiInputConnection};
-use tauri::{Manager, Window, Wry};
-use serde::{Serialize};
+use std::sync::{ Arc, Mutex };
+use midir::{ Ignore, MidiInput, MidiOutput, MidiInputConnection, MidiOutputConnection };
+use tauri::{ Manager, Window, Wry };
+use serde::{ Serialize };
+use std::thread::sleep;
+use std::time::Duration;
 
-pub fn initialize() {
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct MidiNote {
+  pub channel: u8,
+  pub note_number: u8,
+  pub velocity: u8,
+  pub duration: u64,
+  pub cc: (bool, u8, u64),
+}
 
-   
+#[derive(Default)]
+pub struct MidiState {
+  pub input: Mutex<Option<MidiInputConnection<()>>>,
+  pub output: Mutex<Option<MidiOutputConnection>>,
 }
 
 #[tauri::command]
-fn open_midi_connection(
-  midi_state: tauri::State<'_, MidiState>,
-  window: Window<Wry>,
-  input_idx: usize
-) {
-  let handle = Arc::new(window).clone();
-  let mut midi_in = MidiInput::new("My Test Input").unwrap();
-  midi_in.ignore(Ignore::None);
-  let midi_in_ports = midi_in.ports();
-  if let Some(in_port) = midi_in_ports.get(input_idx) {
-    let conn_in = midi_in.connect(in_port, "midir-test", move |stamp, message, log| {
-      // The last of the three callback parameters is the object that we pass in as last parameter of `connect`.
+pub fn init() {
+  //let handle = Arc::new(window).clone();
+  //   let mut midi_in = MidiInput::new("My Test Input").unwrap();
 
-      handle.emit_all("midi_message",  MidiMessage { message: message.to_vec() });
+  //   midi_in.ignore(Ignore::None);
 
-      println!("{}: {:?} (len = {})", stamp, message, message.len());
-    }, ()).unwrap();
-    *midi_state.input.lock().unwrap() = Some(conn_in);
-  }
+  let midi_out = MidiOutput::new("tout").unwrap();
+  // let midi_in_ports = midi_in.ports();
+  let midi_out_ports = midi_out.ports();
+
+  let out_port = midi_out_ports.get(2).ok_or("No MIDI output ports available").unwrap();
+
+  let a = midi_out.port_name(out_port).unwrap();
+  println!("{}", a);
+
+  println!("\nOpening connection");
+  let mut conn_out = midi_out.connect(out_port, "midir-test").unwrap();
+  println!("Connection open. Listen!");
+
+  let mut play_note = |note: u8, duration: u64| {
+    const NOTE_ON_MSG: u8 = 0x90;
+    const NOTE_OFF_MSG: u8 = 0x80;
+    const VELOCITY: u8 = 0x64;
+    // We're ignoring errors in here
+    let _ = conn_out.send(&[NOTE_ON_MSG, note, VELOCITY]);
+    sleep(Duration::from_millis(duration * 150));
+    let _ = conn_out.send(&[NOTE_OFF_MSG, note, VELOCITY]);
+  };
+
+  sleep(Duration::from_millis(4 * 150));
+
+  play_note(66, 4);
+  play_note(65, 3);
+  play_note(63, 1);
+  play_note(61, 6);
+  play_note(59, 2);
+  play_note(58, 4);
+  play_note(56, 4);
+  play_note(54, 4);
+
+  // *midi_state.output.lock().unwrap() = Some(conn_out);
+
+  //   if let Some(in_port) = midi_in_ports.get(input_idx) {
+  //     let conn_in = midi_in.connect(in_port, "midir-test", move |stamp, message, log| {
+
+  //       // The last of the three callback parameters is the object that we pass in as last parameter of `connect`.
+
+  //      // handle.emit_all("midi_message",  MidiMessage { message: message.to_vec() });
+
+  //       println!("{}: {:?} (len = {})", stamp, message, message.len());
+  //     }, ()).unwrap();
+  //     *midi_state.input.lock().unwrap() = Some(conn_in);
+  //   }
 }

51b7f203a4edc8565b72ef4935f251221acca3d9
Jade Rowland <jaderowlanddev@gmail.com>
2023-09-05 00:00:29 -0400

    ideas

--- packages/desktopbridge/oscbridge.mjs
+++ packages/desktopbridge/oscbridge.mjs
@@ -1,16 +1,20 @@
-import { parseNumeral, Pattern } from '@strudel.cycles/core';
+import { parseNumeral, Pattern, logger } from '@strudel.cycles/core';
 import { Invoke } from './utils.mjs';
 
-Pattern.prototype.osc = function () {
+Pattern.prototype.osc = function (otherparams) {
   return this.onTrigger(async (time, hap, currentTime, cps = 1) => {
+    if (otherparams != null && typeof otherparams === 'object') {
+      logger('params must be passed in as a key value object');
+    }
+
     hap.ensureObjectValue();
     const cycle = hap.wholeOrPart().begin.valueOf();
     const delta = hap.duration.valueOf();
-    const controls = Object.assign({}, { cps, cycle, delta }, hap.value);
+    const controls = { ...Object.assign({}, { cps, cycle, delta }, hap.value), ...otherparams };
     // make sure n and note are numbers
     controls.n && (controls.n = parseNumeral(controls.n));
     controls.note && (controls.note = parseNumeral(controls.note));
-
+    console.log(otherparams);
     const params = [];
 
     const timestamp = Math.round(Date.now() + (time - currentTime) * 1000);
@@ -33,6 +37,7 @@ Pattern.prototype.osc = function () {
     if (params.length) {
       messagesfromjs.push({ target: '/dirt/play', timestamp, params });
     }
+    console.log(otherparams);
 
     if (messagesfromjs.length) {
       setTimeout(() => {

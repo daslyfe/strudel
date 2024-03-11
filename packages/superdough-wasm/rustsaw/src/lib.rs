0a2ace5cea1fce46d982ddff326c2c6774028a1f
Jade (Rose) Rowland <jaderowlanddev@gmail.com>
2024-03-10 22:29:50 -0400

    working on it

--- packages/superdough-wasm/rustsaw/src/lib.rs
+++ packages/superdough-wasm/rustsaw/src/lib.rs
@@ -1,6 +1,6 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn saw(t: f64, f: f64) -> f64 {
-    return (((f * t * 1.0) % 1.0) - 0.5) * 1.0;
+pub fn saw(currentTime: f64, sampleRate: f64, frequency: f64) -> f64 {
+    return (((f * currentTime * 1.0) % 1.0) - 0.5) * 1.0;
 }
